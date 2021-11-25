62e9063dd46f661eee9bd915b4985303fd9f6d72
Sven <svenschmidt75@googlemail.com>
2021-11-25 14:52:43 -0700

    add Rust library

---
+++ cargo/src/lib.rs
@@ -0,0 +1,55 @@
+use std::ffi::{CStr, CString};
+use std::os::raw::c_char;
+
+#[no_mangle]
+pub extern "C" fn rust_greeting(to: *const c_char) -> *mut c_char {
+    let c_str = unsafe { CStr::from_ptr(to) };
+    let recipient = match c_str.to_str() {
+        Err(_) => "there",
+        Ok(string) => string,
+    };
+
+    CString::new("Hello ".to_owned() + recipient)
+        .unwrap()
+        .into_raw()
+}
+
+/// Expose the JNI interface for android below
+#[cfg(target_os = "android")]
+#[allow(non_snake_case)]
+pub mod android {
+    extern crate jni;
+
+    use self::jni::objects::{JClass, JString};
+    use self::jni::sys::jstring;
+    use self::jni::JNIEnv;
+    use super::*;
+
+    #[no_mangle]
+    pub unsafe extern "C" fn Java_RustGreetings_00024Companion_greeting(
+        env: JNIEnv,
+        _: JClass,
+        java_pattern: JString,
+    ) -> jstring {
+        println!("Test 1");
+
+        // Our Java companion code might pass-in "world" as a string, hence the name.
+        let world = rust_greeting(
+            env.get_string(java_pattern)
+                .expect("invalid pattern string")
+                .as_ptr(),
+        );
+
+        println!("Test 2");
+
+        // Retake pointer so that we can use it below and allow memory to be freed when it goes out of scope.
+        let world_ptr = CString::from_raw(world);
+        let output = env
+            .new_string(world_ptr.to_str().unwrap())
+            .expect("Couldn't create java string!");
+
+        println!("Test 3");
+
+        output.into_inner()
+    }
+}
